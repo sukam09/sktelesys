0e980ad48e21fa2930a39fef842692f9d8ecb973
sukam09 <sukam09@naver.com>
2018-07-27 16:33:23 +0900

    개꿀~

--- temperature.js
+++ temperature.js
@@ -6,7 +6,8 @@ var SerialPort=require('serialport');
 sp.on('open', function(){
 	//console.log('Serial Port OPEN');
 	sp.on('data', function(data){
-		console.log("Temperature: ", data[0]*5.0/1024.0/0.01, "˚C");
+		var temperature=data[0]*5.0/1024.0/0.01;
+		console.log("Temperature: ", Math.floor(temperature*10)/10, "˚C");
 	});
 });
 
