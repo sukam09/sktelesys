c831025c8d14d1cd5f93c14e9120f23601d67da6
sukam09 <sukam09@naver.com>
2018-07-30 08:51:29 +0900

    파일명 변경

--- monitorsys.js
+++ monitorsys.js
@@ -1,13 +1,11 @@
 var SerialPort=require('serialport');
 	portName='COM1',
-	sp=new SerialPort(portName),
-	sensorVal=0;
+	sp=new SerialPort(portName);
 
 sp.on('open', function(){
-	//console.log('Serial Port OPEN');
 	sp.on('data', function(data){
 		var temperature=data[0]*5.0/1024.0/0.01;
+		console.log()
 		console.log("Temperature: ", Math.floor(temperature*10)/10, "˚C");
 	});
 });
-
