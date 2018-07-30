81bd25b21d9e9f21e8a490adba6ab291defb85bf
sukam09 <sukam09@naver.com>
2018-07-30 09:39:12 +0900

    시간 출력

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var SerialPort=require('serialport');
 	portName='COM1',
 	sp=new SerialPort(portName);
 
+var moment=require('moment');
+
 sp.on('open', function(){
 	sp.on('data', function(data){
 		var temperature=data[0]*5.0/1024.0/0.01;
-		console.log()
-		console.log("Temperature: ", Math.floor(temperature*10)/10, "˚C");
+		var date=moment().format("YYYY-MM-DD HH:mm:ss");
+		console.log(date, Math.floor(temperature*10)/10);
 	});
 });
