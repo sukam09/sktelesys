b97912c902da5007fe5d5848ea32fb6e5def2467
sukam09 <sukam09@naver.com>
2018-07-30 14:41:08 +0900

    시리얼 포트 변경 COM1->COM5

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var SerialPort=require('serialport');
-	portName='COM1',
-	sp=new SerialPort(portName);
-
 var moment=require('moment');
+var io=require('socket.io').listen(8000);
+
+var SerialPort=require('serialport');
+	portName='COM5',
+	sp=new SerialPort(portName);
 
 sp.on('open', function(){
 	sp.on('data', function(data){
+		var date=moment().format('YYYY-MM-DD HH:mm:ss');
 		var temperature=data[0]*5.0/1024.0/0.01;
-		var date=moment().format("YYYY-MM-DD HH:mm:ss");
 		console.log(date, Math.floor(temperature*10)/10);
 	});
 });
