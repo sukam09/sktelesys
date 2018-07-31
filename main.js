591a8c6bd85ccc2f091e80f59020016e07eebd2e
sukam09 <sukam09@naver.com>
2018-07-31 10:57:01 +0900

    최신 코드 이것만 쓸 예정

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
+var io = require('socket.io');
+var fs = require('fs');
+
 var SerialPort=require('serialport');
 	portName='COM5',
 	sp=new SerialPort(portName);
 
 var moment=require('moment');
 
+
+
 sp.on('open', function(){
 	sp.on('data', function(data){
-		var date=moment().format('YYYY-MM-DD HH:mm:ss');
 		var temperature=data[0]*5.0/1024.0/0.01;
-		console.log(date, Math.floor(temperature*10)/10);
+		console.log()
+		console.log("Temperature: ", Math.floor(temperature*10)/10, "˚C");
+		fs.writeFile('file01_async.txt',temperature,'utf-8',function(e){
+			if(e) console.log(e);
+			else console.log('Write Finish!');
+		});
+
+
+	try{
+		fs.writeFileSync('file02_sync.txt',temperature,'utf-8');
+		console.log('Write Fininshed!');
+	}catch(e){
+		console.log(e);
+	}
 	});
-});
+})
