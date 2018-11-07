bf482e57557a963caf154114f5d8dda599f75c0b
sukam09 <sukam09@naver.com>
2018-11-07 23:06:39 +0900

    로그 보고서 버튼

--- openweb.js
+++ openweb.js
@@ -21,7 +21,8 @@ var server = http.createServer(function(request,response){
           response.end(data);
       }
     });
-  }else{
+  }
+  else{
     response.writeHead(404, {'Content-Type':'text/html'});
     response.end('404 Page Not Found');
   }
@@ -30,19 +31,3 @@ var server = http.createServer(function(request,response){
 server.listen(80, function(){
   console.log('Server is running...');
 });
-
-// setInterval(ERRORchecker, 1000);
-
-// function ERRORchecker(){
-//   fs.readFile('file02_sync_RealTime.txt',"utf8", (err, warningdata) => {
-//     if (err) throw err;
-//     console.log(warningdata);
-//     if(warningdata>25){
-//       console.log("EREEEEEEE");
-//     }
-//   });
-
-//   server.listen(80, function(){
-//     console.log('Server is running...');
-//   });
-// }
